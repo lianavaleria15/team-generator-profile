53cb1bf228636cc58383f67f921e1ce3cd01147a
liana.valeria15 <liana.valeria15@gmail.com>
2021-11-22 15:36:11 +0000

    defined tests for employee class

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,5 +1,11 @@
-test("", () => {
+test("Should return name for name input", () => {
   //declare expected
   //get actual
   //assert
 });
+
+test("Should return employee id for id input", () => {});
+
+test("Should return email for email input ", () => {});
+
+test("Should return employee for get role function", () => {});
