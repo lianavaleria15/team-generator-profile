ccfdae24b4878d531dba2c928263a76ce347ce65
liana.valeria15 <liana.valeria15@gmail.com>
2021-11-22 21:40:24 +0000

    added test for name input on employee class

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,11 +1,24 @@
-test("Should return name for name input", () => {
-  //declare expected
-  //get actual
-  //assert
+const Employee = require("../lib/Employee");
+
+describe("Get employee", () => {
+  //add mock class data
+  const mockEmployee = new Employee({
+    name: "James",
+    id: 123,
+    email: "james@here",
+  });
+  test("Should return name for name input", () => {
+    //declare expected
+    const expected = "James";
+    //get actual
+    const actual = mockEmployee.name;
+    //assert
+    expect(actual).toBe(expected);
+  });
+
+  test("Should return employee id for id input", () => {});
+
+  test("Should return email for email input ", () => {});
+
+  test("Should return employee for get role function", () => {});
 });
-
-test("Should return employee id for id input", () => {});
-
-test("Should return email for email input ", () => {});
-
-test("Should return employee for get role function", () => {});
