0c3a4dc23fa0c07a91d8cb0a8f25f546a2b6b335
liana.valeria15 <liana.valeria15@gmail.com>
2021-11-22 22:11:11 +0000

    added comments on test employee

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -8,6 +8,8 @@ describe("Get employee", () => {
     email: "james@here",
     role: "employee",
   });
+
+  //test for name input
   test("Should return name for name input", () => {
     //declare expected
     const expected = "James";
@@ -17,18 +19,21 @@ describe("Get employee", () => {
     expect(actual).toBe(expected);
   });
 
+  //test for id
   test("Should return employee id for id input", () => {
     const expected = 123;
     const actual = mockEmployee.id;
     expect(actual).toBe(expected);
   });
 
+  //test for email
   test("Should return email for email input ", () => {
     const expected = "james@here";
     const actual = mockEmployee.email;
     expect(actual).toBe(expected);
   });
 
+  //test for role
   test("Should return employee for get role function", () => {
     const expected = "employee";
     const actual = mockEmployee.role;
