55a24807ddcfecfe3eed73016ca464cbed342cca
liana.valeria15 <liana.valeria15@gmail.com>
2021-11-23 17:40:22 +0000

    imported employee classes and added new entries

--- source/index.js
+++ source/index.js
@@ -2,18 +2,12 @@
 const inquirer = require("inquirer");
 
 //import classes
-const Employee = require("../lib/Employee");
-
-const Manager = require("../lib/Manager");
-
-const Engineer = require("../lib/Engineer");
-
-const Intern = require("../lib/Intern");
-
+const { Engineer, Intern, Manager } = require("../lib");
 //import team name and employee type questions
+
 const {
   teamName,
-  startQuestions,
+  addNewEmployee,
   employeeType,
 } = require("../source/utils/employeeTypeQuestion");
 
@@ -35,22 +29,40 @@ const start = async () => {
   //get managers answers
   const managerAnswers = await inquirer.prompt(managerQuestions);
   console.log(managerAnswers);
+  const newManager = new Manager();
 
   //while loop to get engineer and intern answers, until selected none
-  const employeeTypeAnswer = await inquirer.prompt(employeeType);
-  console.log(employeeTypeAnswer);
+  let inProgress = true;
 
-  //prompt engineer type question
-  if (employeeTypeAnswer.employeeType === "engineer") {
-    const engineerAnswers = await inquirer.prompt(engineerQuestions);
-    console.log(engineerAnswers);
+  while (inProgress) {
+    const employeeTypeAnswer = await inquirer.prompt(employeeType);
+    console.log(employeeTypeAnswer);
+
+    //prompt engineer type question
+    if (employeeTypeAnswer.employeeType === "engineer") {
+      const engineerAnswers = await inquirer.prompt(engineerQuestions);
+      console.log(engineerAnswers);
+      //create new instance of engineer object
+      const newEngineer = new Engineer();
+    }
+
+    //prompt intern type question
+    if (employeeTypeAnswer.employeeType === "intern") {
+      const internAnswers = await inquirer.prompt(internQuestions);
+      console.log(internAnswers);
+      const newIntern = new Intern();
+    }
+
+    //prompt if another employee should be added
+    const addEmployee = await inquirer.prompt(addNewEmployee);
+    console.log(addEmployee);
+
+    if (!addNewEmployee.addNewEmployee) {
+      //break out of the while loop
+      inProgress = false;
+    }
   }
 
-  //prompt intern type question
-  if (employeeTypeAnswer.employeeType === "intern") {
-    const internAnswers = await inquirer.prompt(internQuestions);
-    console.log(internAnswers);
-  }
   //build answers object
   //generate HTML code
   //write to HTML file
