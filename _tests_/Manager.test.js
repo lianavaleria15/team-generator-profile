e64cb761ef8e0d6b66c29763b11c46c32901feaa
liana.valeria15 <liana.valeria15@gmail.com>
2021-11-23 11:06:37 +0000

    added pseudocode for test units manager superclass

---
+++ _tests_/Manager.test.js
@@ -0,0 +1,13 @@
+//import manager class
+const Manager = require("../lib/Manager");
+
+//describe test suit
+describe("Get manager", () => {
+  //add mock data
+  //add unit tests
+  //verify name input
+  //verify id input
+  //verify email input
+  //verify role input
+  //verify office phone input
+});
