c2b0a127eb768e140c821fae67402553288c61f6
liana.valeria15 <liana.valeria15@gmail.com>
2021-11-23 10:20:14 +0000

    added pseudo code for engineer class test

---
+++ _tests_/Engineer.test.js
@@ -0,0 +1,14 @@
+//
+describe("Get engineer", () => {
+  //verify name input
+  test("Should return James for James input", () => {});
+
+  //verify id input
+  test("Should return 33456 for 33456 input", () => {});
+  //verify email input
+  test("Should return james@email for james@email input ", () => {});
+  //verify github username input
+  test("Should return james23 for james23", () => {});
+  //verify role
+  test("Should return engineer", () => {});
+});
