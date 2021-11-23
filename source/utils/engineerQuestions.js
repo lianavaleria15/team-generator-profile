1efcb444ed7545381e2fe08101472ff68b693307
liana.valeria15 <liana.valeria15@gmail.com>
2021-11-23 14:21:00 +0000

    added inquirer questions for engineer class

---
+++ source/utils/engineerQuestions.js
@@ -0,0 +1,17 @@
+const engineerQuestions = [
+  //get name
+  { type: "input", name: "name", message: "Enter engineer's name:" },
+  //get id
+  { type: "input", name: "id", message: "Enter engineer's id:" },
+  //get email
+  { type: "input", name: "email", message: "Enter engineer's email:" },
+  //get github username
+  {
+    type: "input",
+    name: "github username",
+    message: "Enter engineer's github username:",
+  },
+];
+
+//export engineer questions
+module.exports = engineerQuestions;
