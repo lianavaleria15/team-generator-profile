605d6926711129093e56739f960fbcf4660e030f
liana.valeria15 <liana.valeria15@gmail.com>
2021-11-23 13:26:56 +0000

    added managers inquirer questions

--- source/utils/engineerQuestions.js
+++ source/utils/engineerQuestions.js
@@ -1,16 +1,20 @@
 const managerQuestions = [
   //get name
-  {},
+  { type: "input", name: "name", message: "Enter manager's name:" },
 
   //get id
-  {},
+  { type: "input", name: "id", message: "Enter manager's id:" },
 
   //get email
-  {},
+  { type: "input", name: "email", message: "Enter manager's email:" },
 
   //get office number
-  {},
-
-  //get role
-  {},
+  {
+    type: "input",
+    name: "officeNumber",
+    message: "Enter manager's office number:",
+  },
 ];
+
+//export manager questions
+module.exports = managerQuestions;
