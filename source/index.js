80850f172c51d8ed0f2b245ea0b80de6c90b5974
liana.valeria15 <liana.valeria15@gmail.com>
2021-11-23 13:32:54 +0000

    add fs to prompt manager questions

--- source/index.js
+++ source/index.js
@@ -10,12 +10,20 @@ const Engineer = require("../lib/Engineer");
 
 const Intern = require("../lib/Intern");
 
+//import manager questions
+const managerQuestions = require("../source/utils/engineerQuestions");
+
 //start application
-const start = () => {
+const start = async () => {
   //get team name
   //get managers answers
+  const managerAnswers = await inquirer.prompt(managerQuestions);
+  console.log(managerAnswers);
   //while loop to get engineer and intern answers, until selected none
   //build answers object
   //generate HTML code
   //write to HTML file
 };
+
+//run start
+start();
