6f61de6e3690fb49f5749f6a30c6adb6d41da648
liana.valeria15 <liana.valeria15@gmail.com>
2021-11-23 10:33:03 +0000

    fixed export fs for engineer and expected values, passed tests

--- _tests_/Engineer.test.js
+++ _tests_/Engineer.test.js
@@ -4,7 +4,7 @@ describe("Get engineer", () => {
   //add mock data
   const mockEngineer = new Engineer({
     name: "James",
-    id: "33456",
+    id: 33456,
     email: "james@email",
     role: "engineer",
     github: "james23",
@@ -13,37 +13,37 @@ describe("Get engineer", () => {
   //verify name input
   test("Should return James for James input", () => {
     //expected value
-    const expected = "";
-    const actual = "";
+    const expected = "James";
+    const actual = mockEngineer.name;
     expect(actual).toBe(expected);
   });
 
   //verify id input
   test("Should return 33456 for 33456 input", () => {
     //expected value
-    const expected = "";
-    const actual = "";
+    const expected = 33456;
+    const actual = mockEngineer.id;
     expect(actual).toBe(expected);
   });
   //verify email input
   test("Should return james@email for james@email input ", () => {
     //expected value
-    const expected = "";
-    const actual = "";
+    const expected = "james@email";
+    const actual = mockEngineer.email;
     expect(actual).toBe(expected);
   });
   //verify github username input
   test("Should return james23 for james23", () => {
     //expected value
-    const expected = "";
-    const actual = "";
+    const expected = "james23";
+    const actual = mockEngineer.github;
     expect(actual).toBe(expected);
   });
   //verify role
   test("Should return engineer", () => {
     //expected value
-    const expected = "";
-    const actual = "";
+    const expected = "engineer";
+    const actual = mockEngineer.role;
     expect(actual).toBe(expected);
   });
 });
