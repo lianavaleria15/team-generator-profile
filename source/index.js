1af43835e35db86205d9b6227da95b7b2e4ff82e
liana.valeria15 <liana.valeria15@gmail.com>
2021-11-23 12:06:37 +0000

    added pseudo code for start application fs

--- source/index.js
+++ source/index.js
@@ -1,1 +1,22 @@
+//import inquirer
+const inquirer = require("inquirer");
 
+//import classes
+const Employee = require("../lib/Employee");
+
+const Manager = require("../lib/Manager");
+
+const Engineer = require("../lib/Engineer");
+
+const Intern = require("../lib/Intern");
+
+//start application
+
+const start = () => {
+  //get team name
+  //get managers answers
+  //while loop to get engineer and intern answers, until selected none
+  //build answers object
+  //generate HTML code
+  //write to HTML file
+};
