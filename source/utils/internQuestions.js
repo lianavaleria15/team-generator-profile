05bcb4f737248ab4d0a83451035077b328dbcf66
liana.valeria15 <liana.valeria15@gmail.com>
2021-11-23 14:23:11 +0000

    added inquirer questions for intern

---
+++ source/utils/internQuestions.js
@@ -0,0 +1,17 @@
+const internQuestions = [
+  //get name
+  { type: "input", name: "name", message: "Enter interns's name:" },
+  //get id
+  { type: "input", name: "id", message: "Enter interns's id:" },
+  //get email
+  { type: "input", name: "email", message: "Enter interns's email:" },
+  //get github username
+  {
+    type: "input",
+    name: "school",
+    message: "Enter intern's school:",
+  },
+];
+
+//export engineer questions
+module.exports = internQuestions;
