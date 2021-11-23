4b9bf516600bbd05e82b160b3e0e8760640eb9c2
liana.valeria15 <liana.valeria15@gmail.com>
2021-11-23 10:25:54 +0000

    add pseudo code for engineer class on each property and method

--- _tests_/Engineer.test.js
+++ _tests_/Engineer.test.js
@@ -1,14 +1,49 @@
-//
+//import engineer class
+const Engineer = require("../lib/Engineer");
 describe("Get engineer", () => {
+  //add mock data
+  const mockEngineer = new Engineer({
+    name: "James",
+    id: "33456",
+    email: "james@email",
+    role: "engineer",
+    github: "james23",
+  });
+
   //verify name input
-  test("Should return James for James input", () => {});
+  test("Should return James for James input", () => {
+    //expected value
+    const expected = "";
+    const actual = "";
+    expect(actual).toBe(expected);
+  });
 
   //verify id input
-  test("Should return 33456 for 33456 input", () => {});
+  test("Should return 33456 for 33456 input", () => {
+    //expected value
+    const expected = "";
+    const actual = "";
+    expect(actual).toBe(expected);
+  });
   //verify email input
-  test("Should return james@email for james@email input ", () => {});
+  test("Should return james@email for james@email input ", () => {
+    //expected value
+    const expected = "";
+    const actual = "";
+    expect(actual).toBe(expected);
+  });
   //verify github username input
-  test("Should return james23 for james23", () => {});
+  test("Should return james23 for james23", () => {
+    //expected value
+    const expected = "";
+    const actual = "";
+    expect(actual).toBe(expected);
+  });
   //verify role
-  test("Should return engineer", () => {});
+  test("Should return engineer", () => {
+    //expected value
+    const expected = "";
+    const actual = "";
+    expect(actual).toBe(expected);
+  });
 });
