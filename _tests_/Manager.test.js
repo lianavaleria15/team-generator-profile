3d3b35db6a59d9bbd95af6fb6bd42740ed8c9cf2
liana.valeria15 <liana.valeria15@gmail.com>
2021-11-23 11:09:10 +0000

    added template code for unit tests for manager superclass

--- _tests_/Manager.test.js
+++ _tests_/Manager.test.js
@@ -6,8 +6,33 @@ describe("Get manager", () => {
   //add mock data
   //add unit tests
   //verify name input
+  test("Should return Chris for Chris", () => {
+    const expected = "Christ";
+    const actual = "";
+    expect(actual).toBe(expected);
+  });
   //verify id input
+  test("Should return Chris for Chris", () => {
+    const expected = "Christ";
+    const actual = "";
+    expect(actual).toBe(expected);
+  });
   //verify email input
+  test("Should return Chris for Chris", () => {
+    const expected = "Christ";
+    const actual = "";
+    expect(actual).toBe(expected);
+  });
   //verify role input
+  test("Should return Chris for Chris", () => {
+    const expected = "Christ";
+    const actual = "";
+    expect(actual).toBe(expected);
+  });
   //verify office phone input
+  test("Should return Chris for Chris", () => {
+    const expected = "Christ";
+    const actual = "";
+    expect(actual).toBe(expected);
+  });
 });
