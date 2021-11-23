e41ce665f186d7edbfc294d93ec79555ea2bf660
liana.valeria15 <liana.valeria15@gmail.com>
2021-11-23 10:44:53 +0000

    fixed intern superclass, added mock date; put boilerplate code for tests

---
+++ _tests_/Intern.test.js
@@ -0,0 +1,46 @@
+//import intern superclass
+const Intern = require("../lib/Intern");
+
+//describe tests
+describe("Get intern values", () => {
+  //add mock data
+  const mockIntern = new Intern({
+    name: "Bob",
+    id: 78563,
+    email: "bob@email",
+    role: "intern",
+    school: "Yale",
+  });
+
+  //declare tests
+  //test to verify name input
+  test("Should return Bob for Bob input", () => {
+    const expected = "";
+    const actual = "";
+    expect(actual).toBe(expected);
+  });
+  //test to verify id input
+  test("Should return Bob for Bob input", () => {
+    const expected = "";
+    const actual = "";
+    expect(actual).toBe(expected);
+  });
+  //test to verify email input
+  test("Should return Bob for Bob input", () => {
+    const expected = "";
+    const actual = "";
+    expect(actual).toBe(expected);
+  });
+  //test to verify role input
+  test("Should return Bob for Bob input", () => {
+    const expected = "";
+    const actual = "";
+    expect(actual).toBe(expected);
+  });
+  //test to verify school input
+  test("Should return Bob for Bob input", () => {
+    const expected = "";
+    const actual = "";
+    expect(actual).toBe(expected);
+  });
+});
