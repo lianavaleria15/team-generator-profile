33fbce4cd35498efda016c0435aee281b2aca1bd
liana.valeria15 <liana.valeria15@gmail.com>
2021-11-23 18:21:29 +0000

    added index in utils with questions imported

---
+++ source/utils/index.js
@@ -0,0 +1,11 @@
+//import employee type questions
+const managerQuestions = require("./managerQuestions");
+const engineerQuestions = require("./engineerQuestions");
+const internQuestions = require("./internQuestions");
+
+//export all questions
+module.exports = {
+  managerQuestions,
+  engineerQuestions,
+  internQuestions,
+};
